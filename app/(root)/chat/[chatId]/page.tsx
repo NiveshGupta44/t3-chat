b9c2aa351bf6cd6d202f8ef2371864eda2b51062
Nivesh Gupta <nivesh4472@gmail.com>
2026-06-28 18:07:41 +0530

    feat: refactor MessageViewWithForm component and update dependencies; add system prompt for chat interactions

--- app/(root)/chat/[chatId]/page.tsx
+++ app/(root)/chat/[chatId]/page.tsx
@@ -1,10 +1,10 @@
-import { MessageViewWithForm } from "@/modules/chat/components/messages/message-view-form";
+import  MessageWithForm  from "@/modules/chat/components/messages/message-view-form";
 import React from "react";
 
 const ChatIdPage=  async({params}: {params:Promise<{chatId:string}>})=>{
     const {chatId} = await params;
     return (
-        <MessageViewWithForm chatId={chatId} />
+        <MessageWithForm chatId={chatId} />
     )
 }
 
